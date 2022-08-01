81c297412f15814f5ced9a4711bf9cd26f77e0ee
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2022-08-01 14:11:21 -0600

    feat: Cambio de nombre al .bmp generado

--- SR3.py
+++ SR3.py
@@ -4,7 +4,7 @@ gl.glCreateWindow(4000, 4000)
 
 gl.glClear()
 gl.glColor(0, 0, 1)
-gl.generate('burger.obj', (500, 500), [600, 500])
+gl.generate('burger.obj', (700, 700), [2000, 1000])
 #gl.generate('Trex.obj', (600, 600), [2000, 50])
 
-gl.glFinish("Trex.bmp")
+gl.glFinish("Burger.bmp")
