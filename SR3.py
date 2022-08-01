f1a5d2827e3644b2980c0ff6c057f423f8493f6b
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2022-08-01 13:55:08 -0600

    feat: Dise;o final del SR3

--- SR3.py
+++ SR3.py
@@ -1,9 +1,10 @@
 import gl
 
-gl.glCreateWindow(2000, 2000)
+gl.glCreateWindow(4000, 4000)
 
 gl.glClear()
 gl.glColor(0, 0, 1)
 gl.generate('burger.obj', (500, 500), [600, 500])
+#gl.generate('Trex.obj', (600, 600), [2000, 50])
 
 gl.glFinish("Trex.bmp")
