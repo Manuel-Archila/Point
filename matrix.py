ea3d987988552a56431f02e7c8a13951742e56b6
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2022-08-09 15:25:26 -0600

    feat:Creacion de clase matrix

---
+++ matrix.py
@@ -0,0 +1,7 @@
+class Mat(object):
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = []
+        for i in range(rows):
+            self.data.append([0] * cols)
